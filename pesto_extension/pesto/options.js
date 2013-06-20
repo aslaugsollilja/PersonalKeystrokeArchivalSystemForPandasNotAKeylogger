ddc730a8714fe5fa88307801537a3e68062063b8
SuprDewd <suprdewd@gmail.com>
2013-06-20 22:03:52 +0000

    Added an options page

--- pesto_extension/pesto/options.js
+++ pesto_extension/pesto/options.js
@@ -1,54 +1,27 @@
+
 var restore_options = function () {
-    chrome.storage.sync.get('dict', function (res) {
-        var dict = res["dict"];
-        $("#display_keywords tbody").empty();
+    chrome.storage.sync.get('pesto', function (res) {
+        var pesto = res["pesto"];
 
-        for (key in dict) {
-            if (dict.hasOwnProperty(key)) {
-                var checkbox = $("<input/>").attr("type", "checkbox").change(function (key) {
-                    return function () {
-                        dict[key].active = !dict[key].active; /* checkbox.is(":checked"); */
-                        chrome.storage.sync.set({'dict': dict}, restore_options);
-                    };
-                } (key));
+        $("#username").val(pesto['username']);
+        $("#password").val('password');
+        $("#instance_name").val(pesto['instance_name']);
 
-                if (dict[key].active) checkbox.attr("checked", "checked");
-
-                var remove = $("<button/>").append($("<i/>").addClass("icon-remove icon-white")).click(function (key) {
-                    return function () {
-                        delete dict[key];
-                        chrome.storage.sync.set({'dict': dict}, restore_options);
-                    };
-                } (key)).addClass("btn btn-danger");
-
-                $("<tr/>")
-                    .append($("<td/>").addClass("dk_keyword").text(key))
-                    .append($("<td/>").addClass("dk_expansion").text(dict[key].expansion))
-                    .append($("<td/>").addClass("dk_active").append(checkbox))
-                    .append($("<td/>").addClass("dk_remove").append(remove))
-                    .appendTo($("#display_keywords tbody"));
-            }
-        }
     });
 };
 
 $(document).ready(function () {
     restore_options();
 
-    $("#new_add").click(function () {
+    $("#save_settings").click(function () {
 
-        chrome.storage.sync.get('dict', function (res) {
-            var dict = res.hasOwnProperty("dict") ? res["dict"] : {};
-
-            dict[$("#new_keyword").val()] = {
-                expansion: $("#new_expansion").val(),
-                active: true
-            };
-
-            $("#new_keyword").val("");
-            $("#new_expansion").val("");
-            chrome.storage.sync.set({'dict': dict}, restore_options);
-        });
+        chrome.storage.sync.set({
+            'pesto': {
+                'username': $('#username').val(),
+                'password_hash': new jsSHA($('#username').val() + $('#password').val(), 'TEXT').getHash('SHA-512', 'HEX'),
+                'instance_name': $('#instance_name').val()
+            }
+        }, restore_options);
 
         return false;
     });
